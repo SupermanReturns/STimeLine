920205f57d6d4a993008954f598457f62ff0439a
Superman <703353105@qq.com>
2018-08-07 16:24:31 +0800

    Initial Commit

---
+++ STimeLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STimeLine
+//
+//  Created by Superman on 2018/8/7.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
